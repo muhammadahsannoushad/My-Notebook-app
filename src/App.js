5c1a8aa9ef1d5e6b69950f91736d0a5695903a4a
Muhammad Ahsan <muhammadahsannoushad@gmail.com>
2024-01-02 22:43:40 +0500

    create a navbar rout e with react router dom and Added routes in route.  and import NoteState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-// import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Ahsan
-        </a>
-      </header> */}
-        Learn React with Ahsan
-    </div>
+  <> 
+    <NoteState> 
+     <Navbar/>
+       <Routes>    
+         <Route path='/' Component={Home}/>
+       </Routes> 
+          <Routes>    
+           <Route path='/about' Component={About}/>
+         </Routes> 
+    </NoteState>  
+  </>
   );
 }
 
