9d56e3bddf4c4ff758931e06f5bf211b169c2dda
Muhammad Ahsan <muhammadahsannoushad@gmail.com>
2024-01-10 22:37:04 +0500

    import alert Component and send props message (Alert)

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
+import Alert from './components/Alert';
 
 function App() {
   return (
   <> 
     <NoteState> 
      <Navbar/>
+      <Alert message="This is Amazing React Course"/>
        <Routes>    
          <Route path='/' Component={Home}/>
        </Routes> 
